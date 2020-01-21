a0b20d77d6441790e24b48c328e623315d6ec724
Josh Roy <joshnroy@gmail.com>
2020-01-21 10:58:46 -0500

    stuff

--- train_procgen/train.py
+++ train_procgen/train.py
@@ -12,8 +12,9 @@ from baselines.common.vec_env import (
 from baselines import logger
 from mpi4py import MPI
 import argparse
+import os
+import sys
 
-LOG_DIR = '/tmp/procgen_every1_2'
 
 def main():
     num_envs = 64
@@ -28,17 +29,10 @@ def main():
     timesteps_per_proc = 25_000_000
     use_vf_clipping = True
 
-    parser = argparse.ArgumentParser(description='Process procgen training arguments.')
-    parser.add_argument('--env_name', type=str, default='coinrun')
-    parser.add_argument('--distribution_mode', type=str, default='hard', choices=["easy", "hard", "exploration", "memory", "extreme"])
-    parser.add_argument('--num_levels', type=int, default=0)
-    parser.add_argument('--start_level', type=int, default=0)
-    parser.add_argument('--test_worker_interval', type=int, default=0)
-    parser.add_argument('--disc_coeff', type=int, default=1.0)
+    disc_coeff = float(os.environ["SGE_TASK_ID"]) / 100.
+    LOG_DIR = '/tmp/procgen_jumper_easy_disc_coeff_' + str(disc_coeff)
 
-    args = parser.parse_args()
-
-    test_worker_interval = args.test_worker_interval
+    test_worker_interval = 0
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -49,32 +43,40 @@ def main():
         is_test_worker = comm.Get_rank() % test_worker_interval == (test_worker_interval - 1)
 
     mpi_rank_weight = 0 if is_test_worker else 1
-    num_levels = 0 if is_test_worker else args.num_levels
+    num_levels = 0 if is_test_worker else 200
 
     log_comm = comm.Split(1 if is_test_worker else 0, 0)
     format_strs = ['csv', 'stdout', 'tensorboard'] if log_comm.Get_rank() == 0 else []
     logger.configure(dir=LOG_DIR, format_strs=format_strs)
 
     dist_mode = "easy"
+    env_name = "jumper"
 
     logger.info("creating environment")
-    venv = ProcgenEnv(num_envs=num_envs, env_name=args.env_name, num_levels=200, start_level=0, distribution_mode=dist_mode)
+    venv = ProcgenEnv(num_envs=num_envs, env_name=env_name, num_levels=200, start_level=0, distribution_mode=dist_mode)
+    logger.info("1")
     venv = VecExtractDictObs(venv, "rgb")
+    logger.info("2")
 
     venv = VecMonitor(
         venv=venv, filename=None, keep_buf=100,
     )
+    logger.info("3")
 
     venv = VecNormalize(venv=venv, ob=False)
+    logger.info("4")
 
-    test_venv = ProcgenEnv(num_envs=num_envs, env_name=args.env_name, num_levels=0, start_level=1000, distribution_mode=dist_mode)
+    test_venv = ProcgenEnv(num_envs=num_envs, env_name=env_name, num_levels=0, start_level=1000, distribution_mode=dist_mode)
     test_venv = VecExtractDictObs(test_venv, "rgb")
+    logger.info("5")
 
     test_venv = VecMonitor(
         venv=test_venv, filename=None, keep_buf=100,
     )
+    logger.info("6")
 
     test_venv = VecNormalize(venv=test_venv, ob=False)
+    logger.info("7")
 
     logger.info("creating tf session")
     setup_mpi_gpus()
@@ -84,8 +86,8 @@ def main():
     sess = tf.Session(config=config)
     sess.__enter__()
 
-    # conv_fn = lambda x: build_impala_cnn(x, depths=[16,32,32], emb_size=256)
-    conv_fn = lambda x: nature_cnn(x)
+    conv_fn = lambda x: build_impala_cnn(x, depths=[16,32,32], emb_size=256)
+    # conv_fn = lambda x: nature_cnn(x)
 
     logger.info("training")
     ppo2.learn(
@@ -110,7 +112,7 @@ def main():
         vf_coef=0.5,
         max_grad_norm=0.5,
         eval_env=test_venv,
-        disc_coeff=args.disc_coeff,
+        disc_coeff=disc_coeff,
     )
 
 if __name__ == '__main__':
